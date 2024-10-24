a1d558dc68ac11646fc40d67de4666bcc7bd6aa0
Seno <seno.is.dev@gmail.com>
2024-10-24 23:02:14 +1300

    feat(app): initial pagination

--- src/app/bookmark.service.ts
+++ src/app/bookmark.service.ts
@@ -10,7 +10,284 @@ interface Bookmark {
 })
 export class BookmarkService {
     // List of bookmarks
-    bookmarks: Bookmark[] = []
+    bookmarks: Bookmark[] = [
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google1',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google2',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google3',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google4',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google5',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google6',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+        {
+            name: 'Google7',
+            url: 'https://www.google.com/',
+        },
+    ]
 
     // Get all bookmarks
     handleGetAllBookmarks(): Bookmark[] {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,15 +19,59 @@ export class HomeComponent {
     BookmarkService = inject(BookmarkService)
     // List of bookmarks
     bookmarks: Bookmark[] = []
+    items: Bookmark[] = []
+    list: Bookmark[][] = []
 
     // Handle deleting bookmark
     handleDeleteBookmark(bookmarkName: string) {
-        this.bookmarks = this.bookmarks.filter(
+        this.items = this.items.filter(
             (bookmark) => bookmark.name !== bookmarkName
         )
     }
 
+    pageLength = 1
+    currentPage = 1
+    limit = 20
+    startingAt = 0
+
+    prevPage() {
+        // Decreased page when only
+        // When it's not on the first page
+        if (this.currentPage > 1) {
+            this.currentPage--
+        }
+
+        // Starting item index is the limit times
+        // The previous page (current page minus one)
+        let start = this.limit * (this.currentPage - 1)
+        // End page is starting page plus the limit items count
+        let end = start + this.limit
+
+        this.items = this.bookmarks.slice(start, end)
+    }
+
+    nextPage() {
+        // Preventing going further than the page length
+        if (this.currentPage === this.pageLength) return
+        // For the first page starting from index 0
+        // Plus the limit count
+        let start = this.limit
+        let end = start + this.limit
+        // For subsequent pages
+        // Starting from the limit count times the current page
+        // And end at starting page plus the limit count
+        if (this.currentPage > 1) {
+            start = this.limit * this.currentPage
+            end = start + this.limit
+        }
+
+        this.items = this.bookmarks.slice(start, end)
+        this.currentPage++
+    }
+
     constructor() {
         this.bookmarks = this.BookmarkService.handleGetAllBookmarks()
+        this.pageLength = Math.ceil(this.bookmarks.length / this.limit)
+        this.items = this.bookmarks.slice(0, this.limit)
     }
 }
